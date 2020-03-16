47f0639a284e205a6b584ce434f8041add08bd70
Rocky <rockygitt@gmail.com>
2020-03-16 11:06:54 +0100

    Refactoring

--- modules/cloudant.js
+++ modules/cloudant.js
@@ -1,21 +1,20 @@
+const Cloudant = require('@cloudant/cloudant');
+const appEnv = require('cfenv').getAppEnv(
+  process.env.NODE_ENV === 'production' ? { } : { vcap: require('./vcap-local.json') }
+);
+let cloudant;
+
+if (appEnv.services.cloudantNoSQLDB) {
+  cloudant = Cloudant(appEnv.services.cloudantNoSQLDB[0].credentials);
+} else if (process.env.CLOUDANT_URL) {
+  cloudant = Cloudant(process.env.CLOUDANT_URL);
+}
+
 module.exports = class Cloudant {
 
   constructor(dbName) {
-    const Cloudant = require('@cloudant/cloudant');
-    const appEnv = require('cfenv').getAppEnv(
-      process.env.NODE_ENV === 'production' ? { } : { vcap: require('./vcap-local.json') }
-    );
-
     this.dbName = dbName;
     this.db = null;
-
-    let cloudant;
-
-    if (appEnv.services.cloudantNoSQLDB) {
-      cloudant = Cloudant(appEnv.services.cloudantNoSQLDB[0].credentials);
-    } else if (process.env.CLOUDANT_URL) {
-      cloudant = Cloudant(process.env.CLOUDANT_URL);
-    }
 
     if (cloudant) {
       cloudant.db.create(dbName)
@@ -24,8 +23,6 @@ module.exports = class Cloudant {
 
       this.db = cloudant.db.use(dbName);
     }
-
-    console.log(this.db);
   }
 
   insert(doc) {
@@ -34,13 +31,13 @@ module.exports = class Cloudant {
         doc._id = body.id;
         return doc;
       })
-      .catch(error => console.log(`[${this.dbName}.insert] `, error.message));
+      .catch(error => console.log(`[${this.dbName}.insert]`, error.message));
   }
 
   find(selector = { }, fields = [], sort = []) {
     return this.db.find({ selector, fields, sort })
       .then(body => body.docs)
-      .catch(error => console.log(`[${this.dbName}.find] `, error.message));
+      .catch(error => console.log(`[${this.dbName}.find]`, error.message));
   }
 
 };
