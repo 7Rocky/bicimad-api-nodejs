4f7f4eea3914f3aec047ac86a6393c706fe909b9
Rocky <rockygitt@gmail.com>
2020-03-16 16:14:50 +0100

    Fixes

--- routes/bicimad.routes.js
+++ routes/bicimad.routes.js
@@ -30,7 +30,7 @@ router.get(`${endpoint}/stations/:kind`, async (req, res) => {
     } else if (kind === 'destination') {
       stations = myCache.get(kind) || await bicimadCtrl.getStationsDestination();
     } else {
-      return res.status(404).json({ message: 'Not found' });
+      return res.status(404).json({ error: 'Not found' });
     }
 
     myCache.set(kind, stations);
@@ -46,25 +46,25 @@ router.get(`${endpoint}/movements`, async (req, res) => {
 
   if (date) {
     if (from && to) {
-      movements = myCache.get('date_from_to') ||
+      movements = myCache.get(`date=${date}&from=${from}&to=${to}`) ||
         await bicimadCtrl.getMovementsFromTo(date, Number(from), Number(to));
-      myCache.set('date_from_to', movements);
+      myCache.set(`date=${date}&from=${from}&to=${to}`, movements);
     } else if (from && !to) {
-      movements = myCache.get('date_from') ||
+      movements = myCache.get(`date=${date}&from=${from}`) ||
         await bicimadCtrl.getMovementsFrom(date, Number(from));
-      myCache.set('date_from', movements);
+      myCache.set(`date=${date}&from=${from}`, movements);
     } else if (!from && to) {
-      movements = myCache.get('date_to') ||
+      movements = myCache.get(`date=${date}&to=${to}`) ||
         await bicimadCtrl.getMovementsTo(date, Number(to));
-      myCache.set('date_to', movements);
+      myCache.set(`date=${date}&to`, movements);
     } else {
-      movements = myCache.get('date') || await bicimadCtrl.getMovements(date);
-      myCache.set('date', movements);
+      movements = myCache.get(`date=${date}`) || await bicimadCtrl.getMovements(date);
+      myCache.set(`date=${date}`, movements)
     }
 
     res.json(movements);
   } else {
-    res.status(400).json({ message: 'Date parameter not provided' });
+    res.status(400).json({ error: 'Date parameter not provided' });
   }
 });
 
@@ -73,12 +73,12 @@ router.get(`${endpoint}/movements/time`, async (req, res) => {
   const _in = Number(req.query.in);
 
   if (date && from && to && !isNaN(_in) && _in > 0) {
-    const movements_time = myCache.get('time') || 
+    const movements_time = myCache.get(`date=${date}&from=${from}&to=${to}&in=${_in}&gt=${gt}`) || 
       await bicimadCtrl.getMovementsFromToIn(date, Number(from), Number(to), _in, gt);
-    myCache.set('time', movements_time);
+    myCache.set(`date=${date}&from=${from}&to=${to}&in=${_in}&gt=${gt}`, movements_time);
     res.json(movements_time);
   } else {
-    res.status(400).json({ message: 'Some parameters are wrong' });
+    res.status(400).json({ error: 'Some parameters are wrong' });
   }
 });
 
@@ -94,12 +94,15 @@ const verifyAuth = async (req, res, next) => {
 };
 
 router.post(`${endpoint}/new`, verifyAuth, async (req, res) => {
-  const document = await bicimadCtrl.new(req.body);
-  if (document) {
-    res.status(201).json(document);
-  } else {
-    res.status(400).json({ error: 'Some fields are invalid' });
+  if (req.body) {
+    const document = await bicimadCtrl.new(req.body);
+
+    if (document) {
+       return res.status(201).json(document);
+    }
   }
+
+  res.status(400).json({ error: 'Some fields are invalid' });
 });
 
 module.exports = router;
